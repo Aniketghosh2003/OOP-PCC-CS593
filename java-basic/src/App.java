26280811599f3aea4edf91170174bc38fffb4472
Aniket Ghosh <121872510+Aniketghosh2003@users.noreply.github.com>
2024-07-23 20:35:02 +0530

    jva basic syntax practice uploaded.

---
+++ java-basic/src/App.java
@@ -0,0 +1,191 @@
+
+//import java.util.Arrays;
+import java.util.*;
+
+public class App {
+    public static void printjava() {
+        System.out.println("helloc world");
+    }
+
+    public static void main(String[] args) {
+        // 1st progarm
+        // System.out.println("Hello, World!");
+
+        // variables
+        // String name = "aniket";
+        // int a = 10;
+        // System.out.println(name);
+
+        // datatypes
+
+        // primitive type
+        // int,float,byte,double
+        // byte age = 30;
+        // int phone = 83894444;
+        // long ph = 00000000000000000000L;
+        // float pi = 3.14F;
+
+        // non-primitive type
+        // String name = "Aniket";
+        // System.out.println(name.length());
+
+        // Strings
+        // String n1 = "aniket";
+        // String n2 = "ghosh";
+        // System.out.println(n1+n2);
+        // String name = "aniket";
+        // System.out.println(name.charAt(1));
+        // System.out.println(name.length());
+        // String n2 = name.replace('n', 'm');
+        // System.out.println(n2);
+        // System.out.println(name.substring(0,3));
+
+        // arrays
+        // int[] arr = new int[4];
+        // arr[0] = 34;
+        // arr[1] = 25;
+        // arr[2] = 45;
+        // arr[3] = 50;
+        // System.out.println(arr.length);
+        // System.out.println(arr[0]);
+        // Arrays.sort(arr);
+        // System.out.println(arr[0]);
+        // int[] arr = {45,47,48}
+        // 2d-arrys
+        // int[][] arr = {{45,48,49},{46,48,50}};
+        // System.out.println(arr[0][0]);
+
+        // casting
+        // int a = 15;
+        // int b = a + (int)15.99;
+        // System.out.println(b);
+
+        // constants
+        // final float pi = 3.14F;
+
+        // operators
+        // +,-,/,*,%
+        // = , ++,--
+
+        // math
+        // System.out.println(Math.random(Math.random()));
+
+        // input
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the number :");
+        // int num = sc.nextInt();
+        // System.out.println(num);
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the name :");
+        // String name = sc.next();
+        // System.out.println(name);
+
+        // comparison operators
+        // boolean temp = true;
+        // if (!temp) {
+        // System.out.println("It is true");
+        // }
+        // else{
+        // System.out.println("fuck off");
+        // }
+
+        // logical operators
+        // int a = 40;
+        // int b = 80;
+        // if (a < 50 && b > 50) {
+        // System.out.println("it is right");
+        // } else {
+        // System.out.println("fuck off");
+        // }
+        // int a = 40;
+        // int b = 80;
+        // if (a < 50 || b < 50) {
+        // System.out.println("it is right");
+        // }
+        // else{
+        // System.out.println("fuck off");
+        // }
+        // ! ,
+
+        // switch case
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the day:");
+        // int day = sc.nextInt();
+
+        // switch (day) {
+        // case 1:
+        // System.out.println("sunday");
+        // break;
+
+        // case 2:
+        // System.out.println("monday");
+        // break;
+
+        // case 3:
+        // System.out.println("tuesday");
+        // break;
+
+        // case 4:
+        // System.out.println("wednesday");
+        // break;
+
+        // case 5:
+        // System.out.println("thursday");
+        // break;
+
+        // case 6:
+        // System.out.println("friday");
+        // break;
+
+        // case 7:
+        // System.out.println("saturday");
+        // break;
+
+        // default:
+        // System.out.println("Wrong input!");
+        // break;
+        // }
+
+        // loops
+        // for (int i = 0; i < 10; i++) {
+        // System.out.println(i);
+        // }
+        // int j = 0;
+        // while (j < 10) {
+        // System.out.println(j);
+        // j++;
+        // }
+
+        // int k = 0;
+        // do {
+        // System.out.println(k);
+        // } while (k < 9);
+
+        // break & continue
+        // int i = 0;
+        // while (true) {
+        // if (i == 3) {
+        // i++;
+        // continue;
+        // }
+        // System.out.println(i);
+        // i++;
+        // if (i > 5) {
+        // break;
+        // }
+        // }
+
+        // exception handling
+        // int[] arr = {1,2,3};
+        // try {
+        // System.out.println(arr[5]);
+        // } catch (Exception e) {
+        // // TODO: handle exception
+        // }
+        // System.out.println("hello");
+
+        // function
+        printjava();
+
+    }
+}
